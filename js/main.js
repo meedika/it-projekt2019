d31281695b08757da3d900fb19f8df3aa23e8c35
meedika <janja.face@gmail.com>
2020-01-06 13:53:49 +0100

    read more button
    read more button

--- js/main.js
+++ js/main.js
@@ -1,70 +1,148 @@
-jQuery(document).ready(function( $ ) {
-  $.fn.extend({
-    animateCss: function(animationName, callback) {
-      var animationEnd = (function(el) {
-        var animations = {
-          animation: 'animationend',
-          OAnimation: 'oAnimationEnd',
-          MozAnimation: 'mozAnimationEnd',
-          WebkitAnimation: 'webkitAnimationEnd',
-        };
+jQuery(document).ready(function($) {
+    $.fn.extend({
+        animateCss: function(animationName, callback) {
+            var animationEnd = (function(el) {
+                var animations = {
+                    animation: 'animationend',
+                    OAnimation: 'oAnimationEnd',
+                    MozAnimation: 'mozAnimationEnd',
+                    WebkitAnimation: 'webkitAnimationEnd',
+                };
 
-        for (var t in animations) {
-          if (el.style[t] !== undefined) {
-            return animations[t];
-          }
+                for (var t in animations) {
+                    if (el.style[t] !== undefined) {
+                        return animations[t];
+                    }
+                }
+            })(document.createElement('div'));
+
+            this.addClass('animated ' + animationName).one(animationEnd, function() {
+                $(this).removeClass('animated ' + animationName);
+
+                if (typeof callback === 'function') callback();
+            });
+
+            return this;
+        },
+    })
+
+    /*
+     *Animations
+     */
+
+    $('#services article').on('mouseenter', function(event) {
+        console.log("i did it boois")
+        event.stopPropagation();
+        $(this).find('img').animateCss('rubberBand');
+    });
+
+    $('.logo').on('mouseenter', function(event) {
+        event.stopPropagation();
+        $(this).animateCss('tada');
+    });
+    $('.logo').animateCss('tada');
+
+    /*
+     *Mobile menu
+     */
+
+    $nav = $('#main-navigation');
+
+    $("#menu-btn").on('click', function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+
+        var $this = $(this);
+
+        if ($this.hasClass('is-active')) {
+            //Opened menu
+            $this.removeClass('is-active')
+            $nav.removeClass('active')
+        } else {
+            //Closed menu
+            $this.addClass('is-active')
+            $nav.addClass('active')
         }
-      })(document.createElement('div'));
 
-      this.addClass('animated ' + animationName).one(animationEnd, function() {
-        $(this).removeClass('animated ' + animationName);
+    });
 
-        if (typeof callback === 'function') callback();
-      });
+    /*
+    Our Work
+    */
 
-      return this;
-    },
-  })
+    $("#hair button.readmore").on('click', function(event) {
+        event.stopPropagation();
+        event.preventDefault();
 
-  /*
- *Animations
- */
+        var $this = $(this);
+        var slikice = $("#hair .more");
 
- $('#services article').on('mouseenter', function(event){
-   console.log("i did it boois")
-   event.stopPropagation();
-   $(this).find('img').animateCss('rubberBand');
- });
+        if ($this.hasClass('is-active')) {
+            //Opened menu
+            $this.removeClass('is-active')
+            $this.html ('Show more')
+            slikice.addClass('hidden')
 
-$('.logo').on('mouseenter', function(event){
-  event.stopPropagation();
-  $(this).animateCss('tada');
-  });
-$('.logo').animateCss('tada');
+           
+        } else {
+            //Closed menu
+            $this.addClass('is-active')
+            
+            $this.html ('Show less')
+            slikice.removeClass('hidden')
+        }
 
-/*
- *Mobile menu
- */
 
-$nav = $('#main-navigation');
+    });
 
-$("#menu-btn").on('click', function(event){
-  event.stopPropagation();
-  event.preventDefault();
+ $("#beard button.readmore").on('click', function(event) {
+        event.stopPropagation();
+        event.preventDefault();
 
-    var $this = $(this);
+        var $this = $(this);
+        var slikice = $("#beard .more");
 
-    if ($this.hasClass('is-active')){
-      //Opened menu
-      $this.removeClass('is-active')
-      $nav.removeClass('active')
-    } else {
-      //Closed menu
-      $this.addClass('is-active')
-      $nav.addClass('active')
-    }
+        if ($this.hasClass('is-active')) {
+            //Opened menu
+            $this.removeClass('is-active')
+            $this.html ('Show more')
+            slikice.addClass('hidden')
 
-  });
+           
+        } else {
+            //Closed menu
+            $this.addClass('is-active')
+            
+            $this.html ('Show less')
+            slikice.removeClass('hidden')
+        }
+
+
+    });
+
+$("#moustache button.readmore").on('click', function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+
+        var $this = $(this);
+        var slikice = $("#moustache .more");
+
+        if ($this.hasClass('is-active')) {
+            //Opened menu
+            $this.removeClass('is-active')
+            $this.html ('Show more')
+            slikice.addClass('hidden')
+
+           
+        } else {
+            //Closed menu
+            $this.addClass('is-active')
+            
+            $this.html ('Show less')
+            slikice.removeClass('hidden')
+        }
+
+
+    });
 
 });
-
